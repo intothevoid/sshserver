a315fdada91cf351541f41db58f139e07ca4abb4
Karan Kadam <karank@outlook.com>
2018-01-26 23:23:35 +1030

    Basic listening from main with cancel handler

--- Program.cs
+++ Program.cs
@@ -4,9 +4,27 @@ namespace sshserver
 {
     class Program
     {
+        private static bool s_IsRunning = false;
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.CancelKeyPress += Console_CancelKeyPress;
+
+            Server server = new Server();
+            server.Start();
+
+            while (s_IsRunning)
+            {
+               server.Poll();
+               System.Threading.Thread.Sleep(25); 
+            }
+
+            server.Stop();
+        }
+
+        private static void Console_CancelKeyPress(object sender, ConsoleCancelEventArgs e)
+        {
+            e.Cancel = true;
+            s_IsRunning = false;
         }
     }
 }
