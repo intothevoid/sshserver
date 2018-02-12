498a3cc20cd889c840a39adef0f0426023a28bea
Karan Kadam <karank@outlook.com>
2018-02-12 23:40:14 +1030

    Handle KexDHInit packet, added NewKeys

--- Client.cs
+++ Client.cs
@@ -4,6 +4,7 @@ using System.Collections.Generic;
 using System.Net;
 using System.Net.Sockets;
 using System.Text;
+using KSSHServer.KexAlgorithms;
 using KSSHServer.Packets;
 using Microsoft.CSharp.RuntimeBinder;
 using Microsoft.Extensions.Logging;
@@ -20,6 +21,7 @@ namespace KSSHServer
         private Packets.KexInit _KexInitClientToServer = null;
         private ExchangeContext _ActiveExchangeContext = new ExchangeContext();
         private ExchangeContext _PendingExchangeContext = new ExchangeContext();
+        private byte[] _SessionId = null;
 
         public Client(Socket socket, ILogger logger)
         {
@@ -183,9 +185,95 @@ namespace KSSHServer
 
         private void HandleSpecificPacket(KexDHInit packet)
         {
-                _Logger.LogDebug("Received KexDHInit");
+            _Logger.LogDebug("Received KexDHInit");
 
-                // TODO: Implement Key Exchange!
+            if ((_PendingExchangeContext == null) || (_PendingExchangeContext.KexAlgorithm == null))
+            {
+                throw new InvalidOperationException("Server did not receive SSH_MSG_KEX_INIT as expected.");
+            }
+
+            // 1. C generates a random number x (1 &lt x &lt q) and computes e = g ^ x mod p.  C sends e to S.
+            // 2. S receives e.  It computes K = e^y mod p
+            byte[] sharedSecret = _PendingExchangeContext.KexAlgorithm.DecryptKeyExchange(packet.ClientValue);
+
+            // 2. S generates a random number y (0 < y < q) and computes f = g ^ y mod p.
+            byte[] serverKeyExchange = _PendingExchangeContext.KexAlgorithm.CreateKeyExchange();
+
+            byte[] hostKey = _PendingExchangeContext.HostKeyAlgorithm.CreateKeyAndCertificatesData();
+
+            // H = hash(V_C || V_S || I_C || I_S || K_S || e || f || K)
+            byte[] exchangeHash = ComputeExchangeHash(
+                _PendingExchangeContext.KexAlgorithm,
+                hostKey,
+                packet.ClientValue,
+                serverKeyExchange,
+                sharedSecret);
+
+            if (_SessionId == null)
+                _SessionId = exchangeHash;
+
+            // https://tools.ietf.org/html/rfc4253#section-7.2
+
+            // Initial IV client to server: HASH(K || H || "A" || session_id)
+            // (Here K is encoded as mpint and "A" as byte and session_id as raw
+            // data.  "A" means the single character A, ASCII 65).
+            byte[] clientCipherIV = ComputeEncryptionKey(
+                _PendingExchangeContext.KexAlgorithm,
+                exchangeHash,
+                _PendingExchangeContext.CipherClientToServer.BlockSize,
+                sharedSecret, 'A');
+
+            // Initial IV server to client: HASH(K || H || "B" || session_id)
+            byte[] serverCipherIV = ComputeEncryptionKey(
+                _PendingExchangeContext.KexAlgorithm,
+                exchangeHash,
+                _PendingExchangeContext.CipherServerToClient.BlockSize,
+                sharedSecret, 'B');
+
+            // Encryption key client to server: HASH(K || H || "C" || session_id)
+            byte[] clientCipherKey = ComputeEncryptionKey(
+                _PendingExchangeContext.KexAlgorithm,
+                exchangeHash,
+                _PendingExchangeContext.CipherClientToServer.KeySize,
+                sharedSecret, 'C');
+
+            // Encryption key server to client: HASH(K || H || "D" || session_id)
+            byte[] serverCipherKey = ComputeEncryptionKey(
+                _PendingExchangeContext.KexAlgorithm,
+                exchangeHash,
+                _PendingExchangeContext.CipherServerToClient.KeySize,
+                sharedSecret, 'D');
+
+            // Integrity key client to server: HASH(K || H || "E" || session_id)
+            byte[] clientHmacKey = ComputeEncryptionKey(
+                _PendingExchangeContext.KexAlgorithm,
+                exchangeHash,
+                _PendingExchangeContext.MACAlgorithmClientToServer.KeySize,
+                sharedSecret, 'E');
+
+            // Integrity key server to client: HASH(K || H || "F" || session_id)
+            byte[] serverHmacKey = ComputeEncryptionKey(
+                _PendingExchangeContext.KexAlgorithm,
+                exchangeHash,
+                _PendingExchangeContext.MACAlgorithmServerToClient.KeySize,
+                sharedSecret, 'F');
+
+            // Set all keys we just generated
+            _PendingExchangeContext.CipherClientToServer.SetKey(clientCipherKey, clientCipherIV);
+            _PendingExchangeContext.CipherServerToClient.SetKey(serverCipherKey, serverCipherIV);
+            _PendingExchangeContext.MACAlgorithmClientToServer.SetKey(clientHmacKey);
+            _PendingExchangeContext.MACAlgorithmServerToClient.SetKey(serverHmacKey);
+
+            // Send reply to client!
+            KexDHReply reply = new KexDHReply()
+            {
+                ServerHostKey = hostKey,
+                ServerValue = serverKeyExchange,
+                Signature = _PendingExchangeContext.HostKeyAlgorithm.CreateSignatureData(exchangeHash)
+            };
+
+            Send(reply);
+            Send(new NewKeys());
         }
 
         private void HandleSpecificPacket(KexInit packet)
@@ -218,6 +306,85 @@ namespace KSSHServer
             _Logger.LogDebug($"Selected MACAlgorithmServerToClient: {_PendingExchangeContext.MACAlgorithmServerToClient.Name}");
             _Logger.LogDebug($"Selected CompressionClientToServer: {_PendingExchangeContext.CompressionClientToServer.Name}");
             _Logger.LogDebug($"Selected CompressionServerToClient: {_PendingExchangeContext.CompressionServerToClient.Name}");
+        }
+
+        private void HandleSpecificPacket(NewKeys packet)
+        {
+            _Logger.LogDebug("Received NewKeys");
+
+            _ActiveExchangeContext = _PendingExchangeContext;
+            _PendingExchangeContext = null;
+        }
+
+        private byte[] ComputeExchangeHash(IKexAlgorithm kexAlgorithm, byte[] hostKeyAndCerts, byte[] clientExchangeValue, byte[] serverExchangeValue, byte[] sharedSecret)
+        {
+            // H = hash(V_C || V_S || I_C || I_S || K_S || e || f || K)
+            using (ByteWriter writer = new ByteWriter())
+            {
+                writer.WriteString(_ProtocolVersionExchange);
+                writer.WriteString(ServerConstants.ProtocolVersionExchange);
+
+                writer.WriteBytes(_KexInitClientToServer.GetBytes());
+                writer.WriteBytes(_KexInitServerToClient.GetBytes());
+                writer.WriteBytes(hostKeyAndCerts);
+
+                writer.WriteMPInt(clientExchangeValue);
+                writer.WriteMPInt(serverExchangeValue);
+                writer.WriteMPInt(sharedSecret);
+
+                return kexAlgorithm.ComputeHash(writer.ToByteArray());
+            }
+        }
+
+        private byte[] ComputeEncryptionKey(IKexAlgorithm kexAlgorithm, byte[] exchangeHash, uint keySize, byte[] sharedSecret, char letter)
+        {
+            // K(X) = HASH(K || H || X || session_id)
+
+            // Prepare the buffer
+            byte[] keyBuffer = new byte[keySize];
+            int keyBufferIndex = 0;
+            int currentHashLength = 0;
+            byte[] currentHash = null;
+
+            // We can stop once we fill the key buffer
+            while (keyBufferIndex < keySize)
+            {
+                using (ByteWriter writer = new ByteWriter())
+                {
+                    // Write "K"
+                    writer.WriteMPInt(sharedSecret);
+
+                    // Write "H"
+                    writer.WriteRawBytes(exchangeHash);
+
+                    if (currentHash == null)
+                    {
+                        // If we haven't done this yet, add the "X" and session_id
+                        writer.WriteByte((byte)letter);
+                        writer.WriteRawBytes(_SessionId);
+                    }
+                    else
+                    {
+                        // If the key isn't long enough after the first pass, we need to
+                        // write the current hash as described here:
+                        //      K1 = HASH(K || H || X || session_id)   (X is e.g., "A")
+                        //      K2 = HASH(K || H || K1)
+                        //      K3 = HASH(K || H || K1 || K2)
+                        //      ...
+                        //      key = K1 || K2 || K3 || ...
+                        writer.WriteRawBytes(currentHash);
+                    }
+
+                    currentHash = kexAlgorithm.ComputeHash(writer.ToByteArray());
+                }
+
+                currentHashLength = Math.Min(currentHash.Length, (int)(keySize - keyBufferIndex));
+                Array.Copy(currentHash, 0, keyBuffer, keyBufferIndex, currentHashLength);
+
+                keyBufferIndex += currentHashLength;
+            }
+
+            return keyBuffer;
         }
 
         public void Disconnect()
---
+++ Packets/NewKeys.cs
@@ -0,0 +1,23 @@
+namespace KSSHServer.Packets
+{
+   public class NewKeys : Packet
+    {
+        public override PacketType PacketType
+        {
+            get
+            {
+                return PacketType.SSH_MSG_NEWKEYS;
+            }
+        }
+
+        protected override void InternalGetBytes(ByteWriter writer)
+        {
+            // No data, nothing to write
+        }
+
+        protected override void Load(ByteReader reader)
+        {
+            // No data, nothing to load
+        }
+    } 
+}
